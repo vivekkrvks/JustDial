97fb28b3afb72d104259504a75b710455fb5c836
vivek kumar <vivek.kr.vks@gmail.com>
2022-03-24 18:59:41 +0530

    D

--- client/src/Components/Context/types.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_USER = "LOGIN_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
-export const RESET = "RESET";
-export const DRAWER = "DRAWER";
--- setup/myurl.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURL: "mongodb+srv://vivek:vivek123@cluster0.sknfi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secret: "mystrongsecret",   
-};
